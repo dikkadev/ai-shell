5bc29b33e64494547c80168f42b27ec0d08f4672
Sett17 <raik.rohde@gmail.com>
2023-07-30 23:00:11 +0200

    fix(chat): strip unnecessary prefixes from command message
    This commit modifies the `stripAnswer` function to remove unnecessary prefixes from the command message. It trims prefixes like "Command", "command", "Cmd", "cmd", "Suggested Command", "Suggested command", and "suggested command". Additionally, it trims any leading colon and space. This ensures that the command message is clean and only contains the actual command to be executed.

--- chat/chat.go
+++ chat/chat.go
@@ -3,6 +3,7 @@ package chat
 import (
 	ctx2 "context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/i582/cfmt/cmd/cfmt"
@@ -69,11 +70,26 @@ func (c *Chat) Execute() (string, error) {
 		return "", err
 	}
 	sw.Stop()
-    c.messages = append(c.messages, resp.Choices[0].Message)
+    strippedMessage := resp.Choices[0].Message
+    strippedMessage.Content = stripAnswer(strippedMessage.Content)
+    c.messages = append(c.messages, strippedMessage)
 
-	fmt.Printf("\033[100m%s\033[0m\n", resp.Choices[0].Message.Content)
 
-	return resp.Choices[0].Message.Content, nil
+	fmt.Printf("\033[100m%s\033[0m\n", strippedMessage.Content)
+
+	return strippedMessage.Content, nil
+}
+
+func stripAnswer(cmd string) string {
+    cmd = strings.TrimPrefix(cmd, "Command")
+    cmd = strings.TrimPrefix(cmd, "command")
+    cmd = strings.TrimPrefix(cmd, "Cmd")
+    cmd = strings.TrimPrefix(cmd, "cmd")
+    cmd = strings.TrimPrefix(cmd, "Suggested Command")
+    cmd = strings.TrimPrefix(cmd, "Suggested command")
+    cmd = strings.TrimPrefix(cmd, "suggested command")
+    cmd = strings.TrimLeft(cmd, ": ")
+    return cmd
 }
 
 func (c *Chat) Revise(revision string) (string, error) {
@@ -108,5 +124,5 @@ func (c *Chat) Revise(revision string) (string, error) {
 var SYSTEM_MESSAGE = ai.ChatCompletionMessage{
 	Role: ai.ChatMessageRoleSystem,
 	Content: `As an AI, you are programmed to generate precise console commands based on user instructions for any shell type. These instructions are your main focus. Along with these instructions, you may receive 'context' c.messages containing various details such as the file listing of the current directory, specific contents of files, command history, and the shell identifier. This context should be used as needed to create the most suitable console command, but remember, it may or may not be helpful in generating the command. The user's instruction should always be your primary guide.
-In every situation, your output should be exclusively the console command, with no added text or explanations. Reiterating for clarity, your output, crafted using any relevant user instructions and possibly the provided context, should contain no other information apart from the command itself. Even when the instructions or context might be ambiguous, you are to give it your best try without asking for any additional clarification or providing any explanation - just the console command.`,
+    In every situation, your output should be exclusively the console command, with no added text or explanations. Reiterating for clarity, your output, crafted using any relevant user instructions and possibly the provided context, should contain no other information apart from the command itself. NOT even a prefix like 'Command:' can be present, 100% only the that is to be execued. Even when the instructions or context might be ambiguous, you are to give it your best try without asking for any additional clarification or providing any explanation - just the console command.`,
 }
